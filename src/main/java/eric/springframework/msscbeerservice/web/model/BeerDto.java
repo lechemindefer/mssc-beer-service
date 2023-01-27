44cd305e843bc5decb4f8614d96c9e803a75d140
lechemindefer <dschangjuste@yahoo.fr>
2023-01-27 18:04:22 +0100

    Added validation closes #8

--- src/main/java/eric/springframework/msscbeerservice/web/model/BeerDto.java
+++ src/main/java/eric/springframework/msscbeerservice/web/model/BeerDto.java
@@ -5,6 +5,10 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
@@ -13,15 +17,36 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+
 public class BeerDto {
+
+    @Null
     private UUID id;
+
+    @Null
     private Integer version;
+
+    @Null
     private OffsetDateTime createdDate;
+
+    @Null
     private OffsetDateTime lastModifiedDate;
+
+    @NotBlank
     private String beerName;
+
+    @NotNull
     private BeerStyleEnum beerStyle;
+
+    @Positive
+    @NotNull
     private long upc;
+
+    @Null
     private Integer quantittyOnHand;
+
+    @Positive
+    @NotNull
     private BigDecimal price;
 
 }
